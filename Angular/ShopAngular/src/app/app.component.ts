54b8b6da99eba2c19a3c87cecfcaf7e82d7bacb2
Alberto <bichi1609@gmail.com>
2025-01-29 19:53:38 +0100

    Tutorial tienda Angular - Casa

--- Angular/ShopAngular/src/app/app.component.ts
+++ Angular/ShopAngular/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductListComponent, TopBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Angular/ShopAngular/src/app/product-list/product-list.component.ts
+++ Angular/ShopAngular/src/app/product-list/product-list.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
+import { products } from '../product';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 
 export class ProductListComponent {
+  products = [...products];
 
+  share() {
+    window.alert('The product has been shared!');
+  }
 }
