4aecfd6a7ae5757f3afaa30fc2c27cc9fdf08c68
Alberto <bichi1609@gmail.com>
2025-01-30 12:30:37 +0100

    ShopAngular terminado

--- Angular/ShopAngular/src/app/product-alerts/product-alerts.component.ts
+++ Angular/ShopAngular/src/app/product-alerts/product-alerts.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input } from '@angular/core';
-import { Product } from '../products';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../product';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-product-alerts',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './product-alerts.component.html',
   styleUrl: './product-alerts.component.css'
 })
 export class ProductAlertsComponent {
 
   @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 
 }
--- Angular/ShopAngular/src/app/product-list/product-list.component.ts
+++ Angular/ShopAngular/src/app/product-list/product-list.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { products } from '../product';
 import { NgIf } from '@angular/common';
+import { ProductAlertsComponent } from "../product-alerts/product-alerts.component";
 
 
 @Component({
   selector: 'app-product-list',
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, ProductAlertsComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
@@ -16,5 +17,9 @@ export class ProductListComponent {
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
