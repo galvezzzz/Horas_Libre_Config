a661fa774b83650ad157fd3cdc2a05a3a20309a9
Alberto <bichi1609@gmail.com>
2025-05-05 14:43:23 +0200

    Cambios Recu 2

--- JavaScript/Recu2Practica/script.js
+++ JavaScript/Recu2Practica/script.js
@@ -1,5 +1,6 @@
 window.onload = function () {
 	cargarTabla();
+	document.querySelector("#btnGuardar").addEventListener("click", btnGuardar);
 }
 
 function cargarTabla() {
@@ -22,7 +23,6 @@ function cargarTabla() {
 let filaSeleccionada;
 
 function btnEditar(index) {
-
 	// Resetear formulario
 	const form = document.querySelector("#form");
 	form.reset();
@@ -43,7 +43,7 @@ function btnEditar(index) {
 	// Agregar razas
 	let select = document.querySelector("#select");
 
-	if(animals[index].species === "cat"){
+	if (animals[index].species === "cat") {
 		select.innerHTML = `
 		<option>Siamés</option>
 		<option>Siberiano</option>
@@ -51,7 +51,7 @@ function btnEditar(index) {
 		`
 	};
 
-	if(animals[index].species === "dog"){
+	if (animals[index].species === "dog") {
 		select.innerHTML = `
 		<option>Turco</option>
 		<option>Rottweiler</option>
@@ -59,11 +59,70 @@ function btnEditar(index) {
 		`
 	};
 
-	// Rellenar JSON
-	let jsonText = document.querySelector("#jsonText");
-	jsonText.innerHTML  =`<pre>${JSON.stringify(animals[index], null, 2)}</pre>`;
+	actualizarJson(index);
+
 }
 
-function btnGuardar(){
-	
+function actualizarJson(index) {
+	// Rellenar JSON
+	let jsonText = document.querySelector("#jsonText");
+	jsonText.innerHTML = `<pre>${JSON.stringify(animals[index], null, 2)}</pre>`;
 }
+
+function btnGuardar(e) {
+
+	let raza, sexo, peso, info;
+
+	// Comprobar selección
+	let nombre = document.querySelector("#inputNombre");
+	if (nombre.value != "") {
+
+		// Comprobar raza
+		const select = document.querySelector("#select");
+		raza = select.value;
+		console.log(raza);
+
+		// Comprobar sexo
+		const macho = document.querySelector("#macho");
+		const hembra = document.querySelector("#hembra")
+
+		if (macho.checked) {
+			sexo = macho.value;
+		} else if (hembra.checked) {
+			sexo = hembra.value;
+		} else {
+			alert("Debe seleccionar sexo.")
+			return;
+		}
+
+		console.log(sexo);
+
+		// Comprobar peso
+		const peso = document.querySelector("#peso");
+		let value = e.target.value;
+
+		// Comprobar que tenga al menos 1 revisión
+
+
+		// Comprobar diagnóstico
+
+		actualizarJson(index)
+	} else {
+		alert("Selecciona un animal.");
+	}
+
+}
+
+document.getElementById("peso").addEventListener("input", function (e) {
+	let value = e.target.value;
+
+	// Eliminar todo lo que no sea dígito
+	value = value.replace(/\D/g, "");
+  
+	// Insertar el punto automáticamente después de los primeros dos dígitos
+	if (value.length >= 3) {
+	  value = value.slice(0, 2) + '.' + value.slice(2, 4);
+	} 
+  
+	e.target.value = value;
+});
